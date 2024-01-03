16e04fd2903a6289989d8aa62b0636e93abfd11c
SardorTulatov <sardor12@gmail.com>
2024-01-03 11:43:05 +0500

    abvg

---
+++ main.py
@@ -0,0 +1,70 @@
+# 1-vazifa 3-si
+# a = 27
+# b = 27
+# c = 4
+# print("Tomonlari 27 ga teng kvadratning yuzi" ,a*b,"teng va peremetri esa" ,a*c,"teng")
+# 1-vazifa 4-si
+# a = 8
+# c = 2
+# d = 3.14
+# print("Doira yuzi" ,a**c*d, "ga teng")
+# 1-vazifa 5-si
+# q = 9
+# w = 7
+# print("Katetlari 9 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor formulasi yordamida)" ,q*q+w*w,)
+# 1-vazifa 6-si
+# e = 12
+# r = 45
+# t = 33
+# y = 3
+# print("Keltirilgan sonlarni o'rta arifmetigini toping:12, 45, 33" "Javobi", (e+r+t)/y)
+# 1-vazifa 7-si
+# u = 10
+# i = 18
+# o = 7
+# print("Berilgan parallelipipedning hajmini toping: Uzunligi - 10, kengligi - 18, baladligi - 7 ga teng" ,u*i*o,)
+# 1-vazifa 8-si
+# p = 37
+# f = 69
+# print("Berilgan Uchburchakning 3chi burchagining darajasini toping: 1-burchak =37 daraja, 2-burchak = 69 daraja" ,p+f,)
+# 1-vazifa 9-si
+# g = 180
+# h = 106
+# print(g-h)
+# 1-vazifa 10-si
+# j = 135
+# k = 3
+# print("Teng tomonli uchburchakning perimetri 135ga teng uning tomonlarini uzunligi toping:" ,j/k,)
+# 1-vazifa tugadi ------------------------------------------------------------------------------------------------------------->
+
+
+
+
+# 2-vazifa boshlandi
+# kocha = input("Ko'changizni kiriting: ")
+# print("Siz", kocha.title(), "ko'chasida istiqomad qilasiz")
+# mahalla = input("Mahallangizni kiriting: ")
+# print("Siz", mahalla.title(), "Mahallasida yashaysiz")
+# tuman = input("Tumaningizni kiriting: ")
+# print("Siz", tuman.title(), "tumanida yashaysiz")
+# viloyat = input("Viloyatingizni kiriting: ")
+# print("Hush kelibsiz", viloyat.title())
+# 2-vazifa tugadi
+
+
+# 3-vazifa boshlandi
+# son = int(input("Sonni kiriting:"))
+# print("Kvadrati""-",son*son, )
+# print("Kubi" "-" ,son*son*son,)
+# 3-vazifa tugadi
+
+
+
+
+# 4-vazifa boshlandi
+son1 = int(input("1-sonni kiriting:"))
+son2 = int(input("2-sonni kiriting:"))
+print("+" ,son1+son2,)
+print("-" ,son1-son2,)
+print( "*" ,son1*son2,)
+print( "/" ,son1/son2,)
